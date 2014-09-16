603c8b06e157b714039206346b3518aa12fa9988
phani <phani@heresy.in>
2014-09-16 17:55:57 +0530

    question show

--- www/js/controllers/questions.js
+++ www/js/controllers/questions.js
@@ -1,3 +1,28 @@
-function questionsCtrl($scope, Question) {
+function questionsCtrl($scope, $ionicModal, Question) {
   $scope.questions = Question.query();
+  $ionicModal.fromTemplateUrl('templates/question_detail.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function(question) {
+  	$scope.selectedQuestion = question; 
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 }
