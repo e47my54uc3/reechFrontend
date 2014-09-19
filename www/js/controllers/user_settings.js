e021b70977bfaf5ee0a53e61f9246456e8d53a5e
srinivas <kpsrinivas@gmail.com>
2014-09-19 10:07:24 +0530

    Syntax bug.

--- www/js/controllers/user_settings.js
+++ www/js/controllers/user_settings.js
@@ -1,8 +1,8 @@
-function userSettingsCtrl($scope, UserSetting){	
-  UserSetting.get(angular.extend({'id': 'new'}, $scope.apiParams)), function(data){
-  	$scope.model = data;  	  	
+function userSettingsCtrl($scope, UserSetting){
+  UserSetting.get(angular.extend({'id': 'new'}, $scope.apiParams), function(data){
+  	$scope.model = data;
   	if(data.id == null){
-  		angular.forEach($scope.model, function(value, key){ 
+  		angular.forEach($scope.model, function(value, key){
   			if(key != "id")
   				$scope.model[key] = true;
   		});
@@ -13,14 +13,14 @@ function userSettingsCtrl($scope, UserSetting){
 	$scope.changeSetting = function(){
 		if($scope.model.id == null){
   		UserSetting.save(angular.extend({'user_settings': $scope.model}, $scope.apiParams), function(data){
-  			$scope.model = data.user_settings; 
+  			$scope.model = data.user_settings;
   		});
-  	}else{  			
+  	}else{
   		UserSetting.update(angular.extend({'id': $scope.model.id, 'user_settings': $scope.model}, $scope.apiParams), function(data){
-  			
+
   		});
-  	}  		
-	}	    		
-  
+  	}
+	}
+
 
 }
