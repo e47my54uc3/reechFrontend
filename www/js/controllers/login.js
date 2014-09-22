527f7c95ad716ed8992e610ca80ee4c764794222
phani <phani@heresy.in>
2014-09-22 11:51:20 +0530

    login page

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -1,19 +1,17 @@
 function loginCtrl($scope, $rootScope, $location, Auth, $http){
-	var credentials = {
-      phone_number: '9999999999',
-      password: 'test1234'
-  };
-
-  Auth.login(credentials).then(function(user) {
-		localStorage.currentUser = JSON.stringify(user.user);
-		$rootScope.currentUser = JSON.parse(localStorage.currentUser);
-		$http.defaults.headers.common["X-User-Email"]= $rootScope.currentUser.email;
-		$http.defaults.headers.common["X-User-Token"]= $rootScope.currentUser.authentication_token;
-		$location.path("/questions");
-		console.log(user.user);
-  }, function(error) {
-		console.log("In errors.");
+	$scope.credentials = {};
+	$scope.login = function(){
+		Auth.login($scope.credentials).then(function(user) {
+			localStorage.currentUser = JSON.stringify(user.user);
+			$rootScope.currentUser = JSON.parse(localStorage.currentUser);
+			$http.defaults.headers.common["X-User-Email"]= $rootScope.currentUser.email;
+			$http.defaults.headers.common["X-User-Token"]= $rootScope.currentUser.authentication_token;
+			$location.path("/questions");
+			console.log(user.user);
+  	}, function(error) {
+			console.log("In errors.");
       // Authentication failed...
-  });
-
+  	});
+	}
+	
 }
