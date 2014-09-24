64210fe1db1b4139cafe6e0c8dcc7f56d4d4935b
phani <phani@heresy.in>
2014-09-24 13:15:37 +0530

    categories

--- www/js/controllers/categories.js
+++ www/js/controllers/categories.js
@@ -1,5 +1,14 @@
 function categoriesCtrl($scope, Category, $location){
-	$scope.categories = Category.query();
+	$scope.ceil = window.Math.ceil;
+	Category.query({}, function(response){
+			$scope.categories = response;
+			$scope.allCategories = response;		
+			if(!localStorage.selectedCategoriesIds){
+				$scope.selectAllCategories();						
+			}else{
+				$scope.filterCategories();
+			}	
+	});
 	$scope.loadQuestions = function(categoryId) {
     $location.path("categories/" + categoryId + "/questions");
 	}
