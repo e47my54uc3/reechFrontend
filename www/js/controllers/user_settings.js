cbe4acf0cfa06787973d98ebc07af3b95845d01a
phani <phani@heresy.in>
2014-09-26 19:11:10 +0530

    user settings

--- www/js/controllers/user_settings.js
+++ www/js/controllers/user_settings.js
@@ -1,27 +1,46 @@
-function userSettingsCtrl($scope, $location, UserSetting){	
+function userSettingsCtrl($scope, $location, UserSetting, User){	
   $scope.showBackButton = ($location.path() == "/user_settings");
-  UserSetting.get(angular.extend({'id': 'new'}, $scope.apiParams), function(data){
+  $scope.settings = ["question_gets_answer", "linked_to_new", "solution_gets_hi5", "friend_asks_help", "stared_question_gets_answer", "some_one_grabs_my_solution"];
+  $scope.user = {old_password: "", password: "", password_confirmation: ""};
+  $scope.initializeForNewUser = function(type){    
+    angular.forEach($scope.settings, function(key){
+      $scope.model[type][key] = true;
+    });
+  }
+
+  UserSetting.get({'id': 'new'}, function(data){
   	$scope.model = data;  	  	
   	if(data.id == null){
-  		angular.forEach($scope.model, function(value, key){
-  			if(key != "id")
-  				$scope.model[key] = true;
-  		});
-  		$scope.model['reecher_id'] = localStorage.apiId;
+      $scope.initializeForNewUser('email_settings');
+      $scope.initializeForNewUser('message_settings');	  
   		$scope.changeSetting();
   	}
   });
+  
+
 	$scope.changeSetting = function(){
 		if($scope.model.id == null){
-  		UserSetting.save(angular.extend({'user_settings': $scope.model}, $scope.apiParams), function(data){
+  		UserSetting.save({'user_settings': $scope.model}, function(data){
   			$scope.model = data.user_settings;
   		});
   	}else{
-  		UserSetting.update(angular.extend({'id': $scope.model.id, 'user_settings': $scope.model}, $scope.apiParams), function(data){
+  		UserSetting.update({'id': $scope.model.id, 'user_settings': $scope.model}, function(data){
 
   		});
   	}
 	}
+  $scope.changePassword = function(){
+    User.update({id: $scope.currentUser.id, 
+        user: $scope.user
+      }, function(data) {
+        alert("passwordChanged!");
+      }, function(data) {
+      $scope.isError = true;
+      $scope.user.current_password = "";
+      $scope.user.password = "";
+      $scope.user.password_confirmation = "";
+    });
+  }
 
 
 }
