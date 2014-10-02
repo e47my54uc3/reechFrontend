0d928752e0ac4b7c0ae3e0ddd0af8b6f53b71311
srinivas <kpsrinivas@gmail.com>
2014-10-02 16:22:47 +0530

    Included spinner for app. Using it for facebook.

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -1,4 +1,4 @@
-function loginCtrl($scope, $rootScope, $location, Auth, $http, $window, User){
+function loginCtrl($scope, $rootScope, $location, Auth, $http, $window, User, $cordovaSpinnerDialog){
   if ($rootScope.currentUser) {
     $location.path("/categories");
   }
@@ -8,22 +8,25 @@ function loginCtrl($scope, $rootScope, $location, Auth, $http, $window, User){
   	$scope.facebookLogin = function () {
       if(window.cordova) {
         facebookConnectPlugin.login(["email"], function(response){
-          console.log(JSON.stringify(response));
           if(response.authResponse && response.authResponse.accessToken){
+            $cordovaSpinnerDialog.show();
             User.authorizeFacebook({access_token: response.authResponse.accessToken, device: $rootScope.device}, function(response){
               localStorage.currentUser = JSON.stringify(response);
               $rootScope.currentUser = JSON.parse(localStorage.currentUser);
               $http.defaults.headers.common["X-User-Email"]= $rootScope.currentUser.email;
               $http.defaults.headers.common["X-User-Token"]= $rootScope.currentUser.authentication_token;
               $rootScope.setProfile();
+              $cordovaSpinnerDialog.hide();
               $location.path("/categories");
             });
           }
           else {
-            alert("Not authenticated. Try again later")
+            alert("Not authenticated. Try again later");
+            $rootScope.spinner.hide();
           }
         }, function(error){
-          alert("Something went wrong. Try again later.")
+          alert("Something went wrong. Try again later.");
+          $rootScope.spinner.hide();
         });
       }
   	}
