b12e4338a8765a7e969d4bcdb131359baf9e1bef
srinivas <kpsrinivas@gmail.com>
2014-10-04 13:53:24 +0530

    Audience linking and loading.

--- www/js/controllers/questions.js
+++ www/js/controllers/questions.js
@@ -1,6 +1,6 @@
 function questionsCtrl($scope, $ionicModal, Question, $stateParams, $rootScope, $filter) {
   $scope.selectedQuestion = '';
-  $scope.currentCategory = $stateParams.categoryId ? $stateParams.categoryId : '';  
+  $scope.currentCategory = $stateParams.categoryId ? $stateParams.categoryId : '';
   $scope.currentScope = "all_feed";
   $scope.pageOptions = {page: 1, per_page: 3};
   var index;
@@ -15,24 +15,19 @@ function questionsCtrl($scope, $ionicModal, Question, $stateParams, $rootScope, 
     $scope.fetchQuestions();
   });
 
-  $scope.loadMore = function(){
-    if(($scope.pageOptions.page * $scope.pageOptions.per_page) < $scope.count){
-      $scope.pageOptions.page += 1;
-      $scope.fetchQuestions();
-    }
-  }
+
   $scope.beforeQuestionDetailsModal = function(question_id) {
     $scope.selectedQuestion = question_id;
-  }; 
+  };
   $scope.setStar = function(question_id, stared){
     index = $filter('firstIndex')($scope.questions, {'id': question_id});
     $scope.questions[index].is_starred = stared;
   }
-  $scope.starQuestion = function(question_id){ 
-    Question.starQuestion({'question_id': question_id}, function(response){            
+  $scope.starQuestion = function(question_id){
+    Question.starQuestion({'question_id': question_id}, function(response){
       $rootScope.setProfile();
       $scope.setStar(question_id, response.stared);
-    }); 
-  } 
+    });
+  }
 
 }
