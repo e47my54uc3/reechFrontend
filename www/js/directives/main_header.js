f19cb6616c633d8c23e9bc7f0d070561d67536d2
srinivas <kpsrinivas@gmail.com>
2014-10-06 14:40:22 +0530

    Main header with colors.

--- www/js/directives/main_header.js
+++ www/js/directives/main_header.js
@@ -1,9 +1,9 @@
 reech.directive('mainHeader',function($state){
   return{
     restrict : 'E',
+    scope: true,
     templateUrl: 'templates/main_header.html',
     link: function(scope, element, attrs){
-    	
     }
   }
 });
