843bf1bb5a68c71ba74e5b9f31edcdb03deb0323
Jayaram <jayaram@heresy.in>
2014-10-08 12:01:25 +0530

    displaying question count for category type ALL.

--- www/js/controllers/categories.js
+++ www/js/controllers/categories.js
@@ -2,7 +2,7 @@ function categoriesCtrl($scope, $filter, Category){
 	$scope.ceil = window.Math.ceil;
 	Category.query({}, function(response){
 			$scope.categories = response;
-			$scope.categories.push({id: '', title: 'All'});
+			$scope.categories.push({id: '', title: 'All', question_count: $filter('sum')(response, 'question_count')});
 			$scope.categories = $filter('orderBy')($scope.categories, 'title');
 			localStorage.categories = JSON.stringify(response);	
 			if(!localStorage.selectedCategoriesIds){
