b9afc5dbff9b67dfbf32e196f968e2c62792df5a
phani <phani@heresy.in>
2014-10-17 13:33:01 +0530

    solution with image

--- www/js/controllers/answer_question.js
+++ www/js/controllers/answer_question.js
@@ -1,23 +1,27 @@
-function answerQuestionCtrl($scope, $ionicPlatform, $ionicPopup, $cordovaCamera, Solution, $rootScope) {
+function answerQuestionCtrl($scope, $ionicPlatform, $ionicPopup, $cordovaCamera, Solution, $rootScope, $cordovaFile) {
 
   $scope.model = {is_public: true, solver_id: $scope.currentUser.reecher_id, solver: $scope.currentUser.first_name+" "+$scope.currentUser.last_name};
-  $scope.takePicture = function() {
+  $scope.picture = '';
+  $scope.takePicture = function(type) {
 	    var options = {
-	        quality : 75,
-	        destinationType : Camera.DestinationType.DATA_URL,
-	        sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
+	        quality : 50,
+	        destinationType : Camera.DestinationType.FILE_URI,
 	        allowEdit : true,
 	        encodingType: Camera.EncodingType.JPEG,
-	        targetWidth: 100,
-	        targetHeight: 100,
+	        targetWidth: 300,
+	        targetHeight: 300,
 	        popoverOptions: CameraPopoverOptions,
 	        saveToPhotoAlbum: false
 	    };
+      
+      if(type == 'camera'){
+        options.sourceType = Camera.PictureSourceType.CAMERA;
+      }else if(type == 'gallery'){
+        options.sourceType = Camera.PictureSourceType.PHOTOLIBRARY;
+      }
 
 	    $cordovaCamera.getPicture(options).then(function(imageData) {
-	    	$scope.model.picture = imageData;
-	    	var image = document.getElementById('solutionImage');
-    		image.src = "data:image/jpeg;base64," + "imageData";
+	    	$scope.picture = imageData;
 	    }, function(err) {
 	    	alert(err);
 	    });
@@ -25,19 +29,40 @@ function answerQuestionCtrl($scope, $ionicPlatform, $ionicPopup, $cordovaCamera,
 
   	$scope.submit = function(){
   		$scope.model.question_id = $scope.$parent.selectedQuestion.question.question_id;
-  		Solution.save({'solution': $scope.model}, function(data){
-  			var alertPopup = $ionicPopup.alert({
-     			title: 'Solution',
-     			template: 'Solution submitted!'
-   			});
-   			alertPopup.then(function(res) {
-   	 			$scope.model = "";
-  				$scope.closeAnswerModal();
-          $rootScope.setProfile();
-          $rootScope.$broadcast('answerModal-closed');
-   			});
+      if($scope.picture != ''){
+        var options = new FileUploadOptions();
+          options.fileKey = "picture";
+            options.fileName = "filename.jpg";
+            options.mimeType = "image/jpeg";
+            options.chunkedMode = false;
+            var params = {};
+            params.solution = $scope.model;
+            options.params = params;
 
-  		});
+          $cordovaFile.uploadFile(BaseUrl + 'solutions', $scope.picture, options).then(function(result) {
+            //$cordovaSpinnerDialog.hide();
+            alert("upload success");
+            
+        }, function(error) {
+          //$cordovaSpinnerDialog.hide();
+          alert("An error has occurred: Code = " + error.code);
+          }, function (progress) {
+        });
+      }else{
+    		Solution.save({'solution': $scope.model}, function(data){
+    			var alertPopup = $ionicPopup.alert({
+       			title: 'Solution',
+       			template: 'Solution submitted!'
+     			});
+     			alertPopup.then(function(res) {
+     	 			$scope.model = "";
+    				$scope.closeAnswerModal();
+            $rootScope.setProfile();
+            $rootScope.$broadcast('answerModal-closed');
+     			});
+
+    		});
+      }
   	}
     $ionicPlatform.onHardwareBackButton(function(){
       if($scope.$parent.answerModal)
